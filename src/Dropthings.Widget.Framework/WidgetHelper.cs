a59cf215f55722f14f866383d0a50b6e558d66e2
tanzimsaqib <tanzimsaqib@ccb247a4-f123-11dd-a979-a7afd4e285e1>
2009-06-01 14:42:36 +0000

    ADD: WidgetHelper.RegisterWidgetScript registers dependent client scripts for a widget, so that client side functions are available right away while rendered from server code.

---
+++ src/Dropthings.Widget.Framework/WidgetHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Web.UI;
+using System.Web;
+using System.IO;
+
+namespace Dropthings.Widget.Framework
+{
+    public class WidgetHelper
+    {
+        public static void RegisterWidgetScript(Control controlToBind, string pathToClientScript) 
+        {
+            using (var reader = new StreamReader(HttpContext.Current.Server.MapPath(pathToClientScript)))
+            {
+                var content = reader.ReadToEnd();
+                reader.Close();
+                ScriptManager.RegisterStartupScript(controlToBind, controlToBind.GetType(), controlToBind.ToString() + DateTime.Now.Ticks.ToString(), content, true);
+            }
+        }
+    }
+}
