48123be3d3c8a51dff85dee4887eb2b4a0c2007a
omaralzabir <omaralzabir@ccb247a4-f123-11dd-a979-a7afd4e285e1>
2009-06-16 18:28:51 +0000

    Add script version number on Widget loaded javascripts so that when they change, increasing the version number forces download of new javascripts.

---
+++ src/Dropthings.Web.Util/ConstantHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Dropthings.Web.Util
+{
+    public class ConstantHelper
+    {
+        public static readonly string ScriptVersionNo = ConfigurationManager.AppSettings["ScriptVersionNo"] ?? string.Empty;
+    }
+}
