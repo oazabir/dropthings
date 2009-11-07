b747f862e3095fe8cf94ea8644b0ff3c7dfa65e9
omaralzabir <omaralzabir@ccb247a4-f123-11dd-a979-a7afd4e285e1>
2009-11-07 04:40:29 +0000

    Integration Test Helpers.

---
+++ src/Dropthings.Business.Facade.Tests/Helper/MembershipHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace Dropthings.Business.Facade.Test.Helper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Profile;
+
+    using Dropthings.Web.Framework;
+    using System.Diagnostics;
+
+    internal class MembershipHelper
+    {
+        #region Methods
+
+        [DebuggerStepThrough]
+        public static void UsingNewAnonUser(Action<UserProfile> callback)
+        {
+            UserProfile profile = UserProfile.Create(Guid.NewGuid().ToString(), false) as UserProfile;
+            profile.IsFirstVisit = false;
+            profile.Save();
+
+            try
+            {
+                callback(profile);
+            }
+            finally
+            {
+                ProfileManager.DeleteProfile(profile.UserName);
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ src/Dropthings.Business.Facade.Tests/Helper/SetupHelper.cs
@@ -0,0 +1,65 @@
+﻿namespace Dropthings.Business.Facade.Test.Helper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using System.Diagnostics;
+    using Dropthings.Model;
+    using Dropthings.Business.Facade;
+    using Dropthings.Business.Facade.Context;
+    using Xunit;
+
+    internal class SetupHelper
+    {
+        #region Methods
+
+        [DebuggerStepThrough]
+        public static void UsingNewAnonSetup(string userName, Action<UserSetup> callback)
+        {
+            // Create default page setup which is expected to populate all three columns with widgets
+            //var response = WorkflowTest.Run<FirstVisitWorkflow, UserVisitWorkflowRequest, UserVisitWorkflowResponse>(
+            //    new UserVisitWorkflowRequest { IsAnonymous = true, PageName = "", UserName = userName }
+            //    );
+            var response = new Facade(new AppContext(Guid.NewGuid().ToString(), userName)).FirstVisitHomePage(userName, string.Empty, true, false);
+
+            Assert.NotNull(response.CurrentPage);
+            Assert.NotEqual(0, response.UserPages.Count());
+            Assert.NotNull(response.UserSetting);
+
+            try
+            {
+                callback(response);
+            }
+            finally
+            {
+                // TODO: Clear the setup
+            }
+        }
+
+        [DebuggerStepThrough]
+        public static void UsingNewAnonSetup_ByFacade(string userName, Action<UserSetup> callback)
+        {
+            // Create default page setup which is expected to populate all three columns with widgets
+            using(var facade = new Facade(new AppContext(string.Empty, userName)))
+            {
+                var response = facade.FirstVisitHomePage(userName, string.Empty, true, false);
+                Assert.NotNull(response.CurrentPage);
+                Assert.NotEqual(0, response.UserPages.Count());
+                Assert.NotNull(response.UserSetting);
+
+                try
+                {
+                    callback(response);
+                }
+                finally
+                {
+                    // TODO: Clear the setup
+                }
+            }
+        }
+
+        #endregion Methods
+    }
+}
