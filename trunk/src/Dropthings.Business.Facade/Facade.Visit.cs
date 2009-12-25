a90a90c6ecc1133ceef70f3090eec4112061da9b
omaralzabir <omaralzabir@ccb247a4-f123-11dd-a979-a7afd4e285e1>
2009-12-25 05:54:36 +0000

    Implement transaction in first visit so that if something fails during first visit then user does not end up with partial records.

--- trunk/src/Dropthings.Business.Facade/Facade.Visit.cs
+++ trunk/src/Dropthings.Business.Facade/Facade.Visit.cs
@@ -22,113 +22,123 @@ namespace Dropthings.Business.Facade
 
         public UserSetup FirstVisitHomePage(string userName, string pageTitle, bool isAnonymous, bool isFirstVisitAfterLogin)
         {
-            // If user does not exist, then this is the very *FIRST VISIT* of the user and user
-            // Get template setting that so that we can create pages from templates
-            var response = new UserSetup();
-            var userGuid = this.GetUserGuidFromUserName(userName);
-
-            var userSettingTemplate = GetUserSettingTemplate();
-            SetUserRoles(userName, userSettingTemplate.AnonUserSettingTemplate.RoleNames);
-
-            // Get the template user so that its page setup can be cloned for new user
-            var roleTemplate = GetRoleTemplate(userGuid);
-
-            if (userSettingTemplate.CloneAnonProfileEnabled)
+            return AspectF.Define
+                .Transaction()
+                .Return<UserSetup>(() =>
             {
+                // If user does not exist, then this is the very *FIRST VISIT* of the user and user
+                // Get template setting that so that we can create pages from templates
+                var response = new UserSetup();
+                var userGuid = this.GetUserGuidFromUserName(userName);
+
+                var userSettingTemplate = GetUserSettingTemplate();
+                SetUserRoles(userName, userSettingTemplate.AnonUserSettingTemplate.RoleNames);
+
+                // Get the template user so that its page setup can be cloned for new user
+                var roleTemplate = GetRoleTemplate(userGuid);
+
+                if (userSettingTemplate.CloneAnonProfileEnabled)
+                {
+                    if (!roleTemplate.TemplateUserId.IsEmpty())
+                    {
+                        // Get template user pages so that it can be cloned for new user
+                        var templateUserPages = this.GetPagesOfUser(roleTemplate.TemplateUserId);
+
+                        foreach (Page templatePage in templateUserPages)
+                        {
+                            if (!templatePage.IsLocked)
+                            {
+                                ClonePage(userGuid, templatePage);
+                            }
+                        }
+
+                        if (roleTemplate.TemplateUserId != userGuid)
+                        {
+                            //bring only the locked pages which are not in maintenence mode
+                            response.UserSharedPages = this.pageRepository.GetLockedPagesOfUser(roleTemplate.TemplateUserId, false);
+                        }
+                    }
+                }
+                else
+                {
+                    // Setup some default pages
+                    var page = CreatePage(userGuid, string.Empty, null, 0);
+
+                    if (page != null && page.ID > 0)
+                    {
+                        CreateDefaultWidgetsOnPage(userName, page.ID);
+                        RepeatVisitHomePage(userName, pageTitle, isAnonymous, isFirstVisitAfterLogin);    // non-recursive. this will hit the outter most else block
+                    }
+                    else
+                    {
+                        throw new ApplicationException("First page creation failed");
+                    }
+                }
+
+                var currentPages = this.GetPagesOfUser(userGuid);
+                response.UserPages = currentPages;
+                response.UserSetting = GetUserSetting(userGuid);
+                response.CurrentPage = DecideCurrentPage(userGuid, pageTitle, response.UserSetting.CurrentPageId, isAnonymous, isFirstVisitAfterLogin);
+                response.CurrentUserId = userGuid;
+                response.RoleTemplate = roleTemplate;
+                response.IsRoleTemplateForRegisterUser = CheckRoleTemplateIsRegisterUserTemplate(roleTemplate);
+                return response;
+            });
+        }
+
+        public UserSetup RepeatVisitHomePage(string userName, string pageTitle, bool isAnonymous, bool isFirstVisitAfterLogin)
+        {
+            return AspectF.Define
+                .Transaction()
+                .Return<UserSetup>(() =>
+            {
+                // User is visiting again, so load user's existing page setup
+                var response = new UserSetup();
+                var userGuid = this.GetUserGuidFromUserName(userName);
+
+                var roleTemplate = GetRoleTemplate(userGuid);
+                response.RoleTemplate = roleTemplate;
+                response.IsRoleTemplateForRegisterUser = CheckRoleTemplateIsRegisterUserTemplate(roleTemplate);
+
                 if (!roleTemplate.TemplateUserId.IsEmpty())
                 {
                     // Get template user pages so that it can be cloned for new user
-                    var templateUserPages = this.GetPagesOfUser(roleTemplate.TemplateUserId);
-
-                    foreach (Page templatePage in templateUserPages)
-                    { 
-                        if(!templatePage.IsLocked)
-                        {
-                            ClonePage(userGuid, templatePage);
-                        }
-                    }
-
                     if (roleTemplate.TemplateUserId != userGuid)
                     {
                         //bring only the locked pages which are not in maintenence mode
                         response.UserSharedPages = this.pageRepository.GetLockedPagesOfUser(roleTemplate.TemplateUserId, false);
                     }
                 }
-            }
-            else
-            {
-                // Setup some default pages
-                var page = CreatePage(userGuid, string.Empty, null, 0);
 
-                if (page != null && page.ID > 0)
+                var pages = this.GetPagesOfUser(userGuid);
+
+                if (pages != null && pages.Count() > 0)
                 {
-                    CreateDefaultWidgetsOnPage(userName, page.ID);
-                    RepeatVisitHomePage(userName, pageTitle, isAnonymous, isFirstVisitAfterLogin);    // non-recursive. this will hit the outter most else block
+                    // User has pages
+                    response.UserPages = pages;
+
+                    var userSetting = GetUserSetting(userGuid);
+                    response.CurrentPage = DecideCurrentPage(userGuid, pageTitle, userSetting.CurrentPageId, isAnonymous, isFirstVisitAfterLogin);
+
+                    if (userSetting.CurrentPageId != response.CurrentPage.ID)
+                    {
+                        this.userSettingRepository.Update(userSetting, (setting) =>
+                        {
+                            setting.CurrentPageId = response.CurrentPage.ID;
+                        }, null);
+                    }
+
+                    response.UserSetting = GetUserSetting(userGuid);
+                    response.CurrentUserId = userGuid;
                 }
                 else
                 {
-                    throw new ApplicationException("First page creation failed");
-                }
-            }
-
-            var currentPages = this.GetPagesOfUser(userGuid);
-            response.UserPages = currentPages;
-            response.UserSetting = GetUserSetting(userGuid);
-            response.CurrentPage = DecideCurrentPage(userGuid, pageTitle, response.UserSetting.CurrentPageId, isAnonymous, isFirstVisitAfterLogin);
-            response.CurrentUserId = userGuid;
-            response.RoleTemplate = roleTemplate;
-            response.IsRoleTemplateForRegisterUser = CheckRoleTemplateIsRegisterUserTemplate(roleTemplate);
-            return response;
-        }
-
-        public UserSetup RepeatVisitHomePage(string userName, string pageTitle, bool isAnonymous, bool isFirstVisitAfterLogin)
-        {
-            // User is visiting again, so load user's existing page setup
-            var response = new UserSetup();
-            var userGuid = this.GetUserGuidFromUserName(userName);
-
-            var roleTemplate = GetRoleTemplate(userGuid);
-            response.RoleTemplate = roleTemplate;
-            response.IsRoleTemplateForRegisterUser = CheckRoleTemplateIsRegisterUserTemplate(roleTemplate);
-
-            if (!roleTemplate.TemplateUserId.IsEmpty())
-            {
-                // Get template user pages so that it can be cloned for new user
-                if (roleTemplate.TemplateUserId != userGuid)
-                {
-                    //bring only the locked pages which are not in maintenence mode
-                    response.UserSharedPages = this.pageRepository.GetLockedPagesOfUser(roleTemplate.TemplateUserId, false);
-                }
-            }
-
-            var pages = this.GetPagesOfUser(userGuid);
-            
-            if (pages != null && pages.Count() > 0)
-            {
-                // User has pages
-                response.UserPages = pages;
-
-                var userSetting = GetUserSetting(userGuid);
-                response.CurrentPage = DecideCurrentPage(userGuid, pageTitle, userSetting.CurrentPageId, isAnonymous, isFirstVisitAfterLogin);
-
-                if (userSetting.CurrentPageId != response.CurrentPage.ID)
-                {
-                    this.userSettingRepository.Update(userSetting, (setting) =>
-                    {
-                        setting.CurrentPageId = response.CurrentPage.ID;
-                    }, null);
+                    // User has no pages
+                    response = FirstVisitHomePage(userName, pageTitle, isAnonymous, isFirstVisitAfterLogin);
                 }
 
-                response.UserSetting = GetUserSetting(userGuid);
-                response.CurrentUserId = userGuid;
-            }
-            else
-            {
-                // User has no pages
-                response = FirstVisitHomePage(userName, pageTitle, isAnonymous, isFirstVisitAfterLogin);
-            }
-
-            return response;
+                return response;
+            });
         }
 
         #endregion Methods
---
+++ trunk/src/RunAllRepositoryUnitTests.bat
@@ -0,0 +1,4 @@
+md .\testoutput
+..\thirdparty\xunit\xunit.console.exe .\Dropthings.DataAccess.UnitTest\bin\Debug\Dropthings.DataAccess.UnitTest.dll /html .\testoutput\RepositoryUnitTests.html
+.\testoutput\RepositoryUnitTests.html
+pause
