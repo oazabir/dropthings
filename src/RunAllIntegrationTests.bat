6a2ae53635787647cf266e49a403de70fad9b96f
omaralzabir <omaralzabir@ccb247a4-f123-11dd-a979-a7afd4e285e1>
2009-12-25 05:54:36 +0000

    Implement transaction in first visit so that if something fails during first visit then user does not end up with partial records.

---
+++ src/RunAllIntegrationTests.bat
@@ -0,0 +1,4 @@
+md testoutput
+..\thirdparty\xunit\xunit.console.exe .\Dropthings.Business.Facade.Tests\bin\Debug\Dropthings.Business.Facade.Tests.dll /html .\testoutput\IntegrationTests.html
+.\testoutput\IntegrationTests.html
+pause
