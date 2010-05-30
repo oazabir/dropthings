d36df289801cb7c987799a2d2123915d8d5ec6e1
omaralzabir <omaralzabir@ccb247a4-f123-11dd-a979-a7afd4e285e1>
2010-05-30 07:45:02 +0000

    FIX: Issue 98:	Lock widget instance > widget header is not fully disabled. ADD: ToJson extension method to serialize any object to JSON

---
+++ trunk/src/Dropthings.Web.Util/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace Dropthings.Web.Util
+{
+    public static class ExtensionMethods
+    {        
+        public static string ToJson(this object o, int recursionLimit)
+        {
+            var serializer = new JavaScriptSerializer();
+            serializer.RecursionLimit = recursionLimit;
+            return serializer.Serialize(o);
+        }
+        public static string ToJson(this object o)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(o);
+        }
+    }
+}
