ffc4b3f3b81986c975e19daf182085c17e0ed07e
omaralzabir <omaralzabir@ccb247a4-f123-11dd-a979-a7afd4e285e1>
2010-12-31 15:32:34 +0000

    CHANGE: In FastFlicr, Implement http://www.sohtanaka.com/web-design/fancy-thumbnail-hover-effect-w-jquer/y

--- trunk/src/Dropthings/Widgets/FastFlickrWidget.js
+++ trunk/src/Dropthings/Widgets/FastFlickrWidget.js
@@ -17,7 +17,7 @@ FastFlickrWidget.FLICKR_PHOTO_URL="http://www.flickr.com/photos/";
 
 FastFlickrWidget.prototype = {
 
-    load: function () {
+    load: function() {
         if (null == this.cachedJson) {
             this.pageIndex = 0;
 
@@ -32,39 +32,40 @@ FastFlickrWidget.prototype = {
         }
     },
 
-    onContentLoad: function (xml) {
+    onContentLoad: function(xml) {
         this.xml = xml;
         this.showPhotos();
     },
 
-    getPhotoUrl: function (photoNode, small) {
+    getPhotoUrl: function(photoNode, small) {
         return FastFlickrWidget.FLICKR_SERVER_URL + photoNode.getAttribute("server") + '/' + photoNode.getAttribute("id") + '_' + photoNode.getAttribute("secret") + (small ? "_s.jpg" : "_m.jpg");
     },
 
-    getPhotoTitle: function (photoNode) {
+    getPhotoTitle: function(photoNode) {
         return photoNode.getAttribute("title");
     },
 
-    getPhotoPageUrl: function (photoNode) {
+    getPhotoPageUrl: function(photoNode) {
         return FastFlickrWidget.FLICKR_PHOTO_URL + photoNode.getAttribute('owner') + '/' + photoNode.getAttribute('id');
     },
 
-    showPhotos: function () {
-        var div = $get(this.container);
-        div.innerHTML = "";
-        
+    showPhotos: function() {
+        var div = jQuery('#' + this.container);
+        div.text("");
+
         if (null == this.xml)
-            return (div.innerHTML = "Error occured while loading Flickr feed");
+            return (div.text("Error occured while loading Flickr feed"));
 
         var photos = this.xml.documentElement.getElementsByTagName("photo");
 
         var row = 0, col = 0, count = 0;
 
-        var table = document.createElement("table");
-        table.align = "center";
-        var tableBody = document.createElement("TBODY");
-        table.appendChild(tableBody);
-        var tr;
+        var table = jQuery('<table style="position:relative" border="0" align="center"></table>');
+        var tableBody = jQuery('<tbody></tbody>');
+        table.append(tableBody);
+        div.append(table);
+
+        var tr = null;
 
         for (var i = 0; i < 9; i++) {
             var photo = photos[i + (this.pageIndex * 9)];
@@ -75,36 +76,57 @@ FastFlickrWidget.prototype = {
             }
 
             if (col == 0) {
-                tr = document.createElement("TR");
-                tableBody.appendChild(tr);
+                tr = jQuery('<tr></tr>');
+                tableBody.append(tr);
             }
 
-            var td = document.createElement("TD");
+            var a = jQuery('<a style="position:relative; width: 75px; height:75px;"></a>')
+                            .attr({
+                                href: this.getPhotoPageUrl(photo),
+                                target: "_blank",
+                                title: this.getPhotoTitle(photo)
+                            })
+                            .append(jQuery('<img border="0" style="position:absolute; left: 0; top: 0; width: 75px; height: 75px; background: #f0f0f0; padding: 5px; border: 1px solid #ddd; -ms-interpolation-mode: bicubic; " />')
+                                        .attr({ src: this.getPhotoUrl(photo, false) })
+                            );
+                            
+            tr.append(jQuery('<td style="width: 75px; height: 75px;"></td>').append(a));
 
-            var img = document.createElement("IMG");
-            img.src = this.getPhotoUrl(photo, true);
-            img.style.width = img.style.height = "75px";
-            img.style.border = "none";
+            $(a).hover(function() {
+                $(this).css({ 'z-index': '10' }); /*Add a higher z-index value so this image stays on top*/
+                $(this).find('img').addClass("hover").stop() /* Add class of "hover", then stop animation queue buildup*/
+		            .animate({
+		                marginTop: '-50px', /* The next 4 lines will vertically align this image */
+		                marginLeft: '-50px',
+		                top: '50%',
+		                left: '50%',
+		                width: '150px', /* Set new width */
+		                height: '150px', /* Set new height */
+		                padding: '5px'
+		            }, 200); /* this value of "200" is the speed of how fast/slow this hover animates */
 
-            var a = document.createElement("A");
-            a.href = this.getPhotoPageUrl(photo);
-            a.target = "_blank";
-            a.title = this.getPhotoTitle(photo);
-
-            a.appendChild(img);
-            td.appendChild(a);
-            tr.appendChild(td);
+            }, function() {
+                $(this).css({ 'z-index': '0' }); /* Set z-index back to 0 */
+                $(this).find('img').removeClass("hover").stop()  /* Remove the "hover" class , then stop animation queue buildup*/
+		                .animate({
+		                    marginTop: '0', /* Set alignment back to default */
+		                    marginLeft: '0',
+		                    top: '0',
+		                    left: '0',
+		                    width: '75px', /* Set width back to default */
+		                    height: '75px', /* Set height back to default */
+		                    padding: '5px'
+		                }, 400);
+            });
 
             if (++col == 3) { col = 0; row++ }
 
         }
 
-        div.appendChild(table);
-
         if (this.pageIndex == 0) Utility.nodisplay(this.previousId);
     },
 
-    previous: function () {
+    previous: function() {
         this.pageIndex--;
         this.showPhotos();
 
@@ -114,7 +136,7 @@ FastFlickrWidget.prototype = {
 
     },
 
-    next: function () {
+    next: function() {
         this.pageIndex++;
         this.showPhotos();
         Utility.display(this.previousId, true);
